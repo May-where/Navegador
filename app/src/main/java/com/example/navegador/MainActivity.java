6d50d439b016dc0843d3c3438ff037faa2c3826f
May-where <118573629+May-where@users.noreply.github.com>
2023-05-30 20:48:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/navegador/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.navegador;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    
+    DrawerLayout drawerlayout;
+    MaterialToolbar materialtoolbar;
+    FrameLayout framlayout;
+    NavigationView navigationview;
+
+
+    TextView textview;
+
+    View navitga;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        drawerlayout = findViewById(R.id.drawerlayout);
+        materialtoolbar = findViewById(R.id.materialtoolbar);
+        framlayout = findViewById(R.id.framlayout);
+        navigationview = findViewById(R.id.navigationview);
+        navitga =  navigationview.getHeaderView(0);
+        textview = navitga.findViewById(R.id.textview);
+
+
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this,drawerlayout,materialtoolbar,R.string.close,R.string.open);
+
+
+        
+        drawerlayout.addDrawerListener(toggle);
+
+        navigationview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId()==R.id.Home){
+                    textview.setText("Academia de danza DANILOVA");
+
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    drawerlayout.closeDrawer(GravityCompat.START);
+                }
+                else if (item.getItemId()==R.id.Location){
+                    Toast.makeText(MainActivity.this, "Location", Toast.LENGTH_SHORT).show();
+                    drawerlayout.closeDrawer(GravityCompat.START);
+                }
+
+                else if (item.getItemId()==R.id.phone){
+                    Toast.makeText(MainActivity.this, "phone", Toast.LENGTH_SHORT).show();
+                    drawerlayout.closeDrawer(GravityCompat.START);
+                }
+                else if (item.getItemId()==R.id.HipHop){
+                    Toast.makeText(MainActivity.this, "HipHop", Toast.LENGTH_SHORT).show();
+                    drawerlayout.closeDrawer(GravityCompat.START);
+                }
+                return true;
+            }
+        });
+     materialtoolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+         @Override
+         public boolean onMenuItemClick(MenuItem item) {
+
+             if(item.getItemId()==R.id.Home1){
+                 Fragment(new HomeFragment());
+                 Toast.makeText(MainActivity.this, "Home1", Toast.LENGTH_SHORT).show();
+             } else if (item.getItemId()==R.id.Notification){
+                 Toast.makeText(MainActivity.this, "Notification", Toast.LENGTH_SHORT).show();
+
+             }
+
+             return false;
+         }
+     });
+    }
+
+    private void Fragment (Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.framlayout, fragment);
+        ft.commit();
+
+    }
+
+
+}
